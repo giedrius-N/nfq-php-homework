113ff9bac157fc687a864445a7b127a856d69b71
giedrius-N <giedrius.navickas159@gmail.com>
2023-04-23 14:27:03 +0300

    Added base form to edit page

--- code/src/Controller/EditController.php
+++ code/src/Controller/EditController.php
@@ -3,19 +3,45 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Doctrine\ORM\EntityManagerInterface;
+
 class EditController extends AbstractController
-{
+{	
+	/*
 	#[Route('/edit/{id}', name: 'article_edit')]
+	 */
+	/*
 	public function article_edit(Article $article): Response
-	{
-	
-        return $this->render('pages/edit.html.twig', [
+	{*/
+	/*
+	$form = $this->createFormBuilder($article)
+	    ->add('text', TextType::class)
+      	    ->getForm();
+	 */
+	/*
+	$form = $this->createForm(ArticleType::class, $article);
+	 *//*
+	return $this->render('pages/edit.html.twig', [
 		'controller_name' => 'EditController',
 		'article' => $article,	
         ]);
-	}
+	}*/
+	#[Route('/edit/{id}', name: 'article_edit')]
+	public function article_edit(EntityManagerInterface $em, Article $article):Response
+	{
+		
+		$form = $this->createForm(ArticleFormType::class);
+
+
+		return $this->render('pages/edit.html.twig', [
+			'articleForm' => $form->createView(),
+			'article' => $article,
+		]);
+	}		
 }
--- code/var/cache/dev/annotations.php
+++ code/var/cache/dev/annotations.php
@@ -62,6 +62,10 @@ return [[
 '[C]Symfony%5CBundle%5CFrameworkBundle%5CController%5CAbstractController%23addLink' => 1,
 'Symfony%5CBundle%5CFrameworkBundle%5CController%5CAbstractController%24container' => 0,
 '[C]Symfony%5CBundle%5CFrameworkBundle%5CController%5CAbstractController%24container' => 1,
+'App%5CController%5CEditController' => 0,
+'[C]App%5CController%5CEditController' => 1,
+'App%5CController%5CEditController%23article_edit' => 0,
+'[C]App%5CController%5CEditController%23article_edit' => 1,
 'App%5CController%5CIndexController' => 0,
 '[C]App%5CController%5CIndexController' => 1,
 'App%5CController%5CIndexController%23list' => 0,
@@ -560,6 +564,6 @@ return [[
 ], [
 
 0 => [],
-1 => 1682174654,
+1 => 1682247948,
 
 ]];
